749a4e7deefbb8932de26157a530aeeb536ebd30
Mathias Buus <mathiasbuus@gmail.com>
2014-07-24 19:30:51 +0200

    explaining magic bytes

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ var peek = require('peek-stream')
 var through = require('through2')
 
 var isGzipped = function(data) {
-  if (data.length < 10) return false
-  if (data[0] !== 0x1f && data[1] !== 0x8b) return false
-  if (data[2] !== 8) return false
+  if (data.length < 10) return false // gzip header is 10 bytes
+  if (data[0] !== 0x1f && data[1] !== 0x8b) return false // gzip magic bytes
+  if (data[2] !== 8) return false // is deflating
   return true
 }
 
