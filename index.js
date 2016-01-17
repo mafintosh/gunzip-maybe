9bc31ccb4ba47115af7cf954aaf6977afcb9ec5c
Thomas Watson Steen <w@tson.dk>
2016-01-17 14:03:35 +0100

    standard: add missing space before function parentheses

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ var peek = require('peek-stream')
 var through = require('through2')
 var pumpify = require('pumpify')
 
-var isCompressed = function(data) {
+var isCompressed = function (data) {
   if (data.length < 10) return 0 // gzip header is 10 bytes
   if (data[0] === 0x1f && data[1] === 0x8b && data[2] === 8) return 1 // gzip magic bytes
   if (data[0] === 0x78 && (data[1] === 1 || data[1] === 0x9c || data[1] === 0xda)) return 2 // deflate magic bytes
   return 0
 }
 
-var gunzip = function() {
-  return peek({newline:false, maxBuffer:10}, function(data, swap) {
+var gunzip = function () {
+  return peek({newline:false, maxBuffer:10}, function (data, swap) {
     switch (isCompressed(data)) {
       case 1:
         swap(null, pumpify(zlib.createGunzip(), gunzip()))
--- test.js
+++ test.js
@@ -4,52 +4,52 @@ var concat = require('concat-stream')
 var fs = require('fs')
 var gunzip = require('./')
 
-tape('deflated input', function(t) {
+tape('deflated input', function (t) {
   fs.createReadStream(__filename)
     .pipe(zlib.createDeflate())
     .pipe(gunzip())
-    .pipe(concat(function(data) {
+    .pipe(concat(function (data) {
       t.same(data, fs.readFileSync(__filename))
       t.end()
     }))
 })
 
-tape('deflated multiple times', function(t) {
+tape('deflated multiple times', function (t) {
   fs.createReadStream(__filename)
     .pipe(zlib.createDeflate())
     .pipe(zlib.createDeflate())
     .pipe(gunzip())
-    .pipe(concat(function(data) {
+    .pipe(concat(function (data) {
       t.same(data, fs.readFileSync(__filename))
       t.end()
     }))
 })
 
-tape('gunzipped input', function(t) {
+tape('gunzipped input', function (t) {
   fs.createReadStream(__filename)
     .pipe(zlib.createGzip())
     .pipe(gunzip())
-    .pipe(concat(function(data) {
+    .pipe(concat(function (data) {
       t.same(data, fs.readFileSync(__filename))
       t.end()
     }))
 })
 
-tape('gunzipped multiple times', function(t) {
+tape('gunzipped multiple times', function (t) {
   fs.createReadStream(__filename)
     .pipe(zlib.createGzip())
     .pipe(zlib.createGzip())
     .pipe(gunzip())
-    .pipe(concat(function(data) {
+    .pipe(concat(function (data) {
       t.same(data, fs.readFileSync(__filename))
       t.end()
     }))
 })
 
-tape('regular input', function(t) {
+tape('regular input', function (t) {
   fs.createReadStream(__filename)
     .pipe(gunzip())
-    .pipe(concat(function(data) {
+    .pipe(concat(function (data) {
       t.same(data, fs.readFileSync(__filename))
       t.end()
     }))
