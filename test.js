9452df0fbef2d43d172974223f59c530fb332db1
Thomas Watson Steen <w@tson.dk>
2016-01-17 13:56:23 +0100

    Add deflate support

--- test.js
+++ test.js
@@ -4,6 +4,27 @@ var concat = require('concat-stream');
 var fs = require('fs');
 var gunzip = require('./');
 
+tape('deflated input', function(t) {
+	fs.createReadStream(__filename)
+		.pipe(zlib.createDeflate())
+		.pipe(gunzip())
+		.pipe(concat(function(data) {
+			t.same(data, fs.readFileSync(__filename));
+			t.end();
+		}))
+});
+
+tape('deflated multiple times', function(t) {
+	fs.createReadStream(__filename)
+		.pipe(zlib.createDeflate())
+		.pipe(zlib.createDeflate())
+		.pipe(gunzip())
+		.pipe(concat(function(data) {
+			t.same(data, fs.readFileSync(__filename));
+			t.end();
+		}))
+});
+
 tape('gunzipped input', function(t) {
 	fs.createReadStream(__filename)
 		.pipe(zlib.createGzip())
